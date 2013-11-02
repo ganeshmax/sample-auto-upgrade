d4c73a8e83afd882afd666979be56e6d08208b67
Ganeshji Marwaha <ganeshmax@gmail.com>
2013-11-02 16:48:15 -0400

    Added Grunt and Grunt Shell. Added tasks to debug ios directly from grunt. Integrated Grunt into Webstorm using the Node.js facility. Used the Webstorm terminal to tail the log files. Now, just pressing the play button will build the ios app, run simulator, deploy in ios simulator and debug the app.

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig( {
+        pkg: grunt.file.readJSON('package.json'),
+
+        shell: {
+            prepareIos: {
+                command: 'cordova prepare ios'
+            },
+            compileIos: {
+                command: 'cordova compile ios'
+            },
+            buildIos: {
+                command: 'cordova build ios'
+            },
+            emulateIos: {
+                command: './cordova/emulate',
+                options: {
+                    execOptions: {
+                        cwd: 'platforms/ios'
+                    }
+                }
+            }
+        },
+
+        watch: {
+            html: {
+                files: ['www/src/script/template/**/*.html'],
+                tasks: ['jst']
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-shell');
+
+    grunt.registerTask('debug-ios', ['shell:buildIos', 'shell:emulateIos']);
+    grunt.registerTask('emulate-ios', ['shell:emulateIos']);
+
+};
